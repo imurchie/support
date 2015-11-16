61a79cfd14ac4fa3192c0435ea638a92fa50d7af
Isaac Murchie <isaac@saucelabs.com>
2015-11-16 11:39:36 -0800

    Add redirection

--- eco-883/switch_context.py
+++ eco-883/switch_context.py
@@ -14,7 +14,7 @@ desired_capabilities = {}
 desired_capabilities['platformName'] = 'iOS'
 desired_capabilities['platformVersion'] = '9.0'
 desired_capabilities['browserName'] = 'safari'
-desired_capabilities['deviceName'] = 'iPhone Simulator'
+desired_capabilities['deviceName'] = '=iPhone 6 (9.0)'
 # desired_capabilities['appium-version'] = '1.4.15beta1'
 desired_capabilities['appiumVersion'] = {'appium-url': 'https://github.com/imurchie/support/blob/master/eco-883/appium-v1.4.15beta1.tar.bz2?raw=true'}
 # appiumVersion: {"appium-url": "arbitraryurl"}`
@@ -22,17 +22,24 @@ desired_capabilities['device-orientation'] = 'portrait'
 desired_capabilities['name'] = 'switchingContext'
 # desired_capabilities['proxy'] = {"proxyAutoconfigUrl": "http://127.0.0.1:19876/pac.js", "proxyType":"PAC"}
 
-driver = webdriver.Remote(command_executor = ('http://' + SAUCE_USERNAME + ':' + SAUCE_ACCESS_KEY + '@ondemand.saucelabs.com:80/wd/hub'), desired_capabilities = desired_capabilities)
 # driver = webdriver.Remote(command_executor = ('http://' + SAUCE_USERNAME + ':' + SAUCE_ACCESS_KEY + '@ondemand.saucelabs.com:80/wd/hub'), desired_capabilities = desired_capabilities)
-# driver = webdriver.Remote(command_executor = ('http://localhost:4723/wd/hub'),  desired_capabilities = desired_capabilities)
+# driver = webdriver.Remote(command_executor = ('http://' + SAUCE_USERNAME + ':' + SAUCE_ACCESS_KEY + '@ondemand.saucelabs.com:80/wd/hub'), desired_capabilities = desired_capabilities)
+driver = webdriver.Remote(command_executor = ('http://localhost:4723/wd/hub'),  desired_capabilities = desired_capabilities)
 
 driver.implicitly_wait(30)
 
 
-driver.get("https://www.google.com/")
+# driver.get("https://www.google.com/")
+url = 'http://davmagic.com/PAGES67.html'
+driver.get(url)
 print driver.contexts
 
-# time.sleep(10)
+time.sleep(10)
+
+c_url = driver.current_url
+print c_url
+if (c_url != url):
+  time.sleep(5)
 
 driver.switch_to.context("NATIVE_APP")
 print driver.contexts
